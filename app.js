a388545c218ca46b3f9c6849b597ac708a2cb003
thatnewcarsmell <scalajr@gmail.com>
2018-11-08 09:30:51 -0700

    installing dependencies and outlining basic routes

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const data = require('./data.json')
+const port = 3000;
+
+app.get('/mult', (req, res, next) => {
+
+})
+
+app.get('/divide', (req, res, next) => {
+    
+})
+
+app.listen(port)
